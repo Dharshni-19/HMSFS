1df935638fd659a61b9d5a7cac3ef3531fafa100
Sree Dharshni <sreedharshniilango@gmail.com>
2024-09-27 18:12:39 +0530

    Third commit

--- server/app.js
+++ server/app.js
@@ -187,7 +187,7 @@ app.delete('/issues/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
