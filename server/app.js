632fbfb558eab7d65ab070cd9fcf8c551e458688
Dharshni-19 <158270179+Dharshni-19@users.noreply.github.com>
2024-10-22 12:23:04 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -187,7 +187,7 @@ app.delete('/issues/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
 });
